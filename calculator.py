524e08a4adeb4330727a201f7cf526cfe21a9cba
SzyC7 <101209123+SzyC7@users.noreply.github.com>
2022-04-06 19:35:52 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,14 +2,17 @@ def calculate(x, y, operation):
     operations = {"1": lambda a, b: x + y, "2": lambda a, b: x - y, "3": lambda a, b: x * y, "4": lambda a, b: x/y, "5": lambda a, b: x % y}
     return operations[operation](x, y)
 
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
-    print("Kalkulator")
-    print("1. Dodawanie")
-    print("2. Odejmowanie")
-    print("3. Mnożenie")
-    print("4. Dzielenie")
-    print("5. Procent")
+    logging.info("Kalkulator")
+    logging.info("1. Dodawanie")
+    logging.info("2. Odejmowanie")
+    logging.info("3. Mnożenie")
+    logging.info("4. Dzielenie")
+    logging.info("5. Procent")
     end = False
     while not end:
         a = int(input("Podaj pierwsza liczbę:"))
@@ -22,4 +25,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
