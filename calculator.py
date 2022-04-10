3647751240d37136fa1e01c89c939aafa1dc6445
SzyC7 <101209123+SzyC7@users.noreply.github.com>
2022-04-10 10:26:59 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,24 +2,22 @@ def calculate(x, y, operation):
     operations = {"1": lambda a, b: x + y, "2": lambda a, b: x - y, "3": lambda a, b: x * y, "4": lambda a, b: x/y, "5": lambda a, b: x % y}
     return operations[operation](x, y)
 
-import sys
-import logging
-logging.basicConfig(level=logging.DEBUG)
 
 def main():
-    logging.info("Kalkulator")
-    logging.info("1. Dodawanie")
-    logging.info("2. Odejmowanie")
-    logging.info("3. Mnożenie")
-    logging.info("4. Dzielenie")
-    logging.info("5. Procent")
+    print("Calculator")
+    print("1. Adding")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Percent")
     end = False
     while not end:
         a = int(input("Podaj pierwsza liczbę:"))
         b = int(input("Podaj drugą liczbę:"))
-        działanie = input("Wybierz działanie:")
+        operation = input("Wybierz działanie:")
 
-        print(calculate(a, b, działanie))
+        result = calculate(a, b, operation)
+        logging.info(f"Result: {result}")
 
 
 
